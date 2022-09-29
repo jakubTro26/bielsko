14b558f0ed01b3f8e6713fcd83f4840802820d92
kuba <info@weblead.pl>
2022-09-29 21:43:07 +0200

    commit

--- wp-admin/user-edit.php
+++ wp-admin/user-edit.php
@@ -411,7 +411,7 @@ switch ( $action ) {
 
 
 				<table class="form-table" role="presentation">
-					<tr class="user-user-login-wrap p">
+					<tr class="user-user-login-wrap pe">
 						<th><label for="user_login"><?php _e( 'Username' ); ?></label></th>
 						<td><input type="text" name="user_login" id="user_login" value="<?php echo esc_attr( $profile_user->user_login ); ?>" disabled="disabled" class="regular-text" /> <span class="description"><?php _e( 'Usernames cannot be changed.' ); ?></span></td>
 					</tr>
