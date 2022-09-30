928d785dfe97874874c368d6623bc658460349ee
kuba <info@weblead.pl>
2022-09-30 13:47:19 +0200

    commit

--- wp-admin/user-new.php
+++ wp-admin/user-new.php
@@ -245,7 +245,7 @@ Please click the following link to confirm the invite:
 			} else {
 				$redirect = add_query_arg( array( 'update' => 'newuserconfirmation' ), 'user-new.php' );
 			}
-			wp_redirect( $redirect );
+			//wp_redirect( $redirect );
 			die();
 		}
 	}
