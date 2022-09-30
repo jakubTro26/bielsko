bd728ab4aabcbdad86c9f87058849c3286cc6c6a
kuba <info@weblead.pl>
2022-09-30 14:21:18 +0200

    commit

--- wp-admin/user-new.php
+++ wp-admin/user-new.php
@@ -214,42 +214,42 @@ Please click the following link to confirm the invite:
 		if ( is_wp_error( $user_details['errors'] ) && $user_details['errors']->has_errors() ) {
 			$add_user_errors = $user_details['errors'];
 		} else {
-			/** This filter is documented in wp-includes/user.php */
-			$new_user_login = apply_filters( 'pre_user_login', sanitize_user( wp_unslash( $_REQUEST['user_login'] ), true ) );
-			if ( isset( $_POST['noconfirmation'] ) && current_user_can( 'manage_network_users' ) ) {
-				add_filter( 'wpmu_signup_user_notification', '__return_false' );  // Disable confirmation email.
-				add_filter( 'wpmu_welcome_user_notification', '__return_false' ); // Disable welcome email.
-			}
-			wpmu_signup_user(
-				$new_user_login,
-				$new_user_email,
-				array(
-					'add_to_blog' => get_current_blog_id(),
-					'new_role'    => $_REQUEST['role'],
-				)
-			);
-			if ( isset( $_POST['noconfirmation'] ) && current_user_can( 'manage_network_users' ) ) {
-				$key      = $wpdb->get_var( $wpdb->prepare( "SELECT activation_key FROM {$wpdb->signups} WHERE user_login = %s AND user_email = %s", $new_user_login, $new_user_email ) );
-				$new_user = wpmu_activate_signup( $key );
-				if ( is_wp_error( $new_user ) ) {
-					$redirect = add_query_arg( array( 'update' => 'addnoconfirmation' ), 'user-new.php' );
-				} elseif ( ! is_user_member_of_blog( $new_user['user_id'] ) ) {
-					$redirect = add_query_arg( array( 'update' => 'created_could_not_add' ), 'user-new.php' );
-				} else {
-					$redirect = add_query_arg(
-						array(
-							'update'  => 'addnoconfirmation',
-							'user_id' => $new_user['user_id'],
-						),
-						'user-new.php'
-					);
-				}
-			} else {
-				$redirect = add_query_arg( array( 'update' => 'newuserconfirmation' ), 'user-new.php' );
-			}
-			wp_redirect( $redirect );
-			die();
-		}
+		// 	/** This filter is documented in wp-includes/user.php */
+		// 	$new_user_login = apply_filters( 'pre_user_login', sanitize_user( wp_unslash( $_REQUEST['user_login'] ), true ) );
+		// 	if ( isset( $_POST['noconfirmation'] ) && current_user_can( 'manage_network_users' ) ) {
+		// 		add_filter( 'wpmu_signup_user_notification', '__return_false' );  // Disable confirmation email.
+		// 		add_filter( 'wpmu_welcome_user_notification', '__return_false' ); // Disable welcome email.
+		// 	}
+		// 	wpmu_signup_user(
+		// 		$new_user_login,
+		// 		$new_user_email,
+		// 		array(
+		// 			'add_to_blog' => get_current_blog_id(),
+		// 			'new_role'    => $_REQUEST['role'],
+		// 		)
+		// 	);
+		// 	if ( isset( $_POST['noconfirmation'] ) && current_user_can( 'manage_network_users' ) ) {
+		// 		$key      = $wpdb->get_var( $wpdb->prepare( "SELECT activation_key FROM {$wpdb->signups} WHERE user_login = %s AND user_email = %s", $new_user_login, $new_user_email ) );
+		// 		$new_user = wpmu_activate_signup( $key );
+		// 		if ( is_wp_error( $new_user ) ) {
+		// 			$redirect = add_query_arg( array( 'update' => 'addnoconfirmation' ), 'user-new.php' );
+		// 		} elseif ( ! is_user_member_of_blog( $new_user['user_id'] ) ) {
+		// 			$redirect = add_query_arg( array( 'update' => 'created_could_not_add' ), 'user-new.php' );
+		// 		} else {
+		// 			$redirect = add_query_arg(
+		// 				array(
+		// 					'update'  => 'addnoconfirmation',
+		// 					'user_id' => $new_user['user_id'],
+		// 				),
+		// 				'user-new.php'
+		// 			);
+		// 		}
+		// 	} else {
+		// 		$redirect = add_query_arg( array( 'update' => 'newuserconfirmation' ), 'user-new.php' );
+		// 	}
+		// 	wp_redirect( $redirect );
+		// 	die();
+		// }
 	}
 }
 
